32c8171d07cd68d46b5ffa240e3043de52e6af1d
Takayuki Ohtake <takayuki.ohtake@gaiax.com>
2020-06-30 19:33:37 +0900

    ローカルサーバーで銀行口座の登録をテストする
    ```sh
    cd functions/ && npm start
    curl -X POST http://localhost:8080 -d "account_number=0001234" -d "branch_number=000" -d "bank_number=1100" -d "account_holder_name=カ）ヤマダ"
    ```

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,48 @@
 const functions = require('firebase-functions');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.helloWorld = functions
+// TODO: 関数の再試行を考慮する
+// https://firebase.google.com/docs/functions/retries?hl=ja
+
+exports.createExternalAccount = functions
   .region('asia-northeast1')
-  .https.onRequest((request, response) => {
-    response.send("Hello from Firebase! " + process.env.message);
-})
+  .https.onRequest(async (request, response) => {
+    if (request.method !== 'POST') {
+      return Promise.reject(Error())
+    }
+
+    // TODO: バリデーション
+
+    let account = await stripe.accounts.create(
+      {
+        type: 'custom',
+        country: 'JP',
+        requested_capabilities: [
+          'card_payments',
+          'transfers',
+        ],
+      }
+    );
+
+    // TODO: リクエストボディのアカウントIDを使う
+    // let body = await stripe.accounts.createExternalAccount(request.body.account.id, {
+
+    // TODO: BubbleのAPI Connector向けに、成功時と失敗時のbodyの構造を合わせる（`Initialize call`対応）
+    let body = await stripe.accounts.createExternalAccount(account.id, {
+      external_account: {
+        object: 'bank_account',
+        country: 'JP',
+        currency: 'jpy',
+        default_for_currency: true,
+        account_number: request.body.account_number,
+        routing_number: request.body.bank_number + request.body.branch_number,
+        account_holder_name: request.body.account_holder_name
+      }
+    }).then(account => {
+      return account
+    }).catch(error => {
+      console.log(error)
+      return error
+    })
+    response.send(body);
+  })
