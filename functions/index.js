3dc183981018950152a77ccbebbd6c5d52ffc316
Takayuki Ohtake <takayuki.ohtake@gaiax.com>
2020-07-02 19:34:48 +0900

    bubble.ioのAPI Connectorから叩くことを意識した実装
    ローカルでテストしただけの段階
    
    ```sh
    curl -X POST http://localhost:8080 -d "branch_number=000" -d "bank_number=1100" -d "account_holder_name=カ）ヤマダ" -d "account_number=0001234" -d "app_user_id=1234"
    ```

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,79 @@
 const functions = require('firebase-functions');
 const stripe = require('stripe')(functions.config().stripe.secret_key);
 
+function info(accountId, appUserId, message) {
+  console.log('account_id=' + accountId + ',app_user_id=' + appUserId + ': ' + message)
+}
+
+function error(accountId, appUserId) {
+  console.error('account_id=' + accountId + ',app_user_id=' + appUserId + ': ' + message)
+}
+
 // TODO: 関数の再試行を考慮する
 // https://firebase.google.com/docs/functions/retries?hl=ja
 
 exports.createExternalAccount = functions
   .region('asia-northeast1')
   .https.onRequest(async (request, response) => {
+    let createdAccount = await stripe.accounts.create({
+      type: 'custom',
+      country: 'JP',
+      requested_capabilities: [
+        'card_payments',
+        'transfers',
+      ],
+    })
+    let accountId = createdAccount.id
+    // TODO: リクエストボディのaccount_idを使う
+    // let accountId = request.body.account_id
+
+    info(accountId, request.body.app_user_id, 'Start')
+
     if (request.method !== 'POST') {
-      return Promise.reject(Error())
+      info(accountId, request.body.app_user_id, 'Finalize')
+      return new Promise.reject(new Error('Invalid request'))
     }
 
-    // TODO: バリデーション
-
-    let account = await stripe.accounts.create(
-      {
-        type: 'custom',
+    // Bubble APIに対して成功時とエラー時で共通のデータ構造を返す。
+    // API Connectorは「`Initialize call` したときのレスポンス」のデータ構造しか扱えない。
+    let body = {
+      external_account: {
+        id: undefined,
+        object: 'bank_account',
+        account: undefined,
+        account_holder_name: undefined,
+        account_holder_type: undefined,
+        bank_name: undefined,
         country: 'JP',
-        requested_capabilities: [
-          'card_payments',
-          'transfers',
-        ],
-      }
-    );
+        currency: 'jpy',
+        default_for_currency: undefined,
+        fingerprint: undefined,
+        last4: undefined,
+        // metadataのデータ構造を変更する度、BubbleのAPI Connectorで `Reinitialize` すること
+        metadata: {
+          user_id: undefined
+        },
+        routing_number: undefined,
+        status: undefined
+      },
+      // Stripe APIエラーレスポンスの `raw.param`
+      error_param: undefined
+    }
 
-    // TODO: リクエストボディのアカウントIDを使う
-    // let body = await stripe.accounts.createExternalAccount(request.body.account.id, {
+    let account = await stripe.accounts.retrieve(accountId)
+      .catch(error => {
+        body.error_param = 'account_id'
+        error(accountId, request.body.app_user_id, error)
+        return error
+      })
+    let isRetrieved = typeof (account.id) === 'string'
+    if (!isRetrieved) {
+      response.send(body)
+      error(accountId, request.body.app_user_id, 'Finalize')
+      return Promise.reject(new Error('Stripe account not be found: ' + accountId))
+    }
 
-    // TODO: BubbleのAPI Connector向けに、成功時と失敗時のbodyの構造を合わせる（`Initialize call`対応）
-    let body = await stripe.accounts.createExternalAccount(account.id, {
+    await stripe.accounts.createExternalAccount(accountId, {
       external_account: {
         object: 'bank_account',
         country: 'JP',
@@ -36,13 +81,33 @@ exports.createExternalAccount = functions
         default_for_currency: true,
         account_number: request.body.account_number,
         routing_number: request.body.bank_number + request.body.branch_number,
-        account_holder_name: request.body.account_holder_name
+        account_holder_name: request.body.account_holder_name,
+        metadata: {
+          user_id: request.body.app_user_id
+        }
       }
-    }).then(account => {
-      return account
+    }).then(externalAccount => {
+      info(accountId, request.body.app_user_id, 'Created external_account: ' + externalAccount.id)
+      body.external_account = externalAccount
+      return externalAccount
     }).catch(error => {
-      console.log(error)
+      error(accountId, request.body.app_user_id, error)
+      if (error.raw.code === 'account_invalid') {
+        body.error_param = 'account_id'
+      }
+      switch (error.raw.param) {
+        case 'external_account[account_number]':
+          body.error_param = 'external_account[account_number]'
+          break;
+        case 'external_account[routing_number]':
+          body.error_param = 'external_account[routing_number]'
+          break;
+        case 'external_account[account_holder_name]':
+          body.error_param = 'external_account[account_holder_name]'
+          break;
+      }
       return error
     })
-    response.send(body);
+    response.send(body)
+    info(accountId, request.body.app_user_id, 'End')
   })
